58310349caa46a0472a4f47156a2bc1b07453de1
m0n4rch4y0u <156986363+m0n4rch4y0u@users.noreply.github.com>
2024-11-14 16:57:16 +0300

    Update 7 DZ.py

--- 7 DZ.py
+++ 7 DZ.py
@@ -2,11 +2,12 @@ from random import *
 
 n = int(input("Введите размерность массива: "))
 print()
-A = [randint(1, 100) for i in range(n)]
-print("Массив А:", A)
+
+arrayA = [randint(1, 100) for i in range(n)]
+print("Массив А:", arrayA)
 
 s = 0
 for x in range(n):
-    s += A[x]
-sred = s / len(A)
+    s += arrayA[x]
+sred = s / len(arrayA)
 print('Среднее арифметическое массива:', sred)
